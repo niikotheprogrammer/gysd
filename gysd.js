ce2b91066a4ba0cf4c407e3bd3dc7b427254e7b2
niikotheprogrammer <it2m3niko@gmail.com>
2025-08-26 15:17:52 -0300

    Add files via upload

---
+++ gysd.js
@@ -0,0 +1,21 @@
+const mySubmit= document.getElementById("mySubmit");
+const helloH1 = document.getElementById("hello-h1");
+const names = ["friend", "traveler", "adventurer", "visitor"];
+const randomName = names[Math.floor(Math.random() * names.length)];
+helloH1.textContent = `Hello, ${randomName}!!`;
+
+window.addEventListener("load", () => {
+    const loading = document.getElementById("loading");
+
+    setTimeout(() => {
+        loading.classList.add("fade-out");
+        setTimeout(() => {
+            document.body.classList.add("show-elements");
+            document.body.style.overflow = "auto";
+        }, 800);
+    }, 1500);
+});
+
+window.addEventListener("load", playAnimation);
+document.getElementById("mySubmit").addEventListener("click", playAnimation);
+
